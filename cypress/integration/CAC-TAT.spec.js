e530109fa0db2c6a6aef6af2a2ff706a26b965bc
Lucas <abe.bolzani@gmail.com>
2024-08-07 14:54:07 -0300

    Atualizacao de testes

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -98,6 +98,108 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.contains('.button', 'Enviar').click()
         cy.get('.success').should('be.visible', 'success')
     })
+
+    it('seleciona um produto (YouTube) por seu texto', function() {
+  
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('#firstName').type('Lucas')
+        cy.get('#lastName').type('Abe Bolzani')
+        cy.get('#email').type('lucas-bolzani@hotmail.com')
+        cy.get('#product').select('YouTube').should('have.value','youtube')
+        
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', function() {
+  
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('#firstName').type('Lucas')
+        cy.get('#lastName').type('Abe Bolzani')
+        cy.get('#email').type('lucas-bolzani@hotmail.com')
+        cy.get('#product').select('mentoria').should('have.value','mentoria')
+        
+    })
+
+    it('seleciona um produto (Blog) por seu índice', function() {
+  
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('#firstName').type('Lucas')
+        cy.get('#lastName').type('Abe Bolzani')
+        cy.get('#email').type('lucas-bolzani@hotmail.com')
+        cy.get('#product').select(1).should('have.value','blog')
+        
+    })
+
+    it('marca o tipo de atendimento "Feedback"', function() {
+  
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('#firstName').type('Lucas')
+        cy.get('#lastName').type('Abe Bolzani')
+        cy.get('#email').type('lucas-bolzani@hotmail.com')
+        cy.get('#product').select(1).should('have.value','blog')
+        cy.get(':nth-child(4) > input').check().should('have.value','feedback')
+        
+    })
+
+    it('marca cada tipo de atendimento', function() {
+  
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('#firstName').type('Lucas')
+        cy.get('#lastName').type('Abe Bolzani')
+        cy.get('#email').type('lucas-bolzani@hotmail.com')
+        cy.get('#product').select(1).should('have.value','blog')
+        cy.get('#support-type > :nth-child(2) > input').check().should('be.checked','ajuda')
+        cy.get(':nth-child(3) > input').check().should('be.checked','elogio')
+        cy.get(':nth-child(4) > input').check().should('be.checked','feedback')
+        
+    })
+
+    it('marca ambos checkboxes, depois desmarca o último', function() {
+  
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('input[type="checkbox"]').check().last().uncheck().should('not.be.checked')
+          
+    })
+
+    it('marca cada tipo de atendimento', function() {
+  
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('#firstName').type('Lucas')
+        cy.get('#lastName').type('Abe Bolzani')
+        cy.get('#email').type('lucas-bolzani@hotmail.com')
+        cy.get('#product').select(1).should('have.value','blog')
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+        cy.get('input[type="checkbox"]').check().last().uncheck().should('not.be.checked')
+        cy.get('.button').click()
+        cy.get('.error').should('be.visible')
+
+    })
+
+    it('seleciona um arquivo da pasta fixture', function() {
+  
+        cy.get('input[type="file"]').selectFile('./cypress/fixtures/example.json').should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+
+    })
+
+    it('seleciona um arquivo simulando um drag-and-drop', function() {
+  
+        cy.get('input[type="file"]').selectFile('./cypress/fixtures/example.json', {action: 'drag-drop'} ).should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', function() {
+  
+        cy.fixture('example.json').as('sampleFile')
+        cy.get('input[type="file"]').selectFile('@sampleFile').should(function($input) {
+            expect($input[0].files[0].name).to.equal('example.json')
+        })
+
+    })
+
+    
 })
 
   
